2a98163a885a4aa68cf1f5d059191e84b2a70004
Esther <e.orlandeli@aluno.ifsp.edu.br>
2025-06-19 23:17:21 -0300

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,12 @@
+function mais(){
+    numero = parseInt(document.getElementById("numero").innerText)
+    resultado=numero+1
+    document.getElementById("numero").innerHTML = resultado;
+
+}
+function menos(){
+    numero = parseInt(document.getElementById("numero").innerText)
+    resultado=numero-1
+    document.getElementById("numero").innerHTML = resultado;
+
+}
